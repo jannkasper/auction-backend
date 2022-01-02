ad1403e6c6c8f26ebee0b66dd42441e7ea265867
Jan Kasper <jann.kasper@gmail.com>
2022-01-02 18:07:38 +0100

    Security Token

--- src/main/java/com/jannkasper/auction/config/SecurityConfig.java
+++ src/main/java/com/jannkasper/auction/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.jannkasper.auction.config;
 
+import com.jannkasper.auction.filter.JWTTokenGeneratorFilter;
+import com.jannkasper.auction.filter.JWTTokenValidatorFilter;
+import com.jannkasper.auction.filter.RequestValidationBeforeFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -19,9 +25,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().ignoringAntMatchers("/contact", "/notices", "/h2-console/*")
-        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-        .and().authorizeHttpRequests()
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
+        cors().configurationSource(new CorsConfigurationSource() {
+          @Override
+          public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setExposedHeaders(Arrays.asList("Authorization"));
+            config.setMaxAge(3600L);
+            return config;
+          }
+        }).and().csrf().disable()
+        .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+        .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+        .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+        .authorizeRequests()
         .antMatchers("/myAccount").authenticated()
         .antMatchers("/myLoans").authenticated()
         .antMatchers("/myBalance").authenticated()
@@ -32,22 +53,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .and().formLogin().and().httpBasic();
 
     http.headers().frameOptions().disable();
-  }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    return new CorsConfigurationSource() {
-      @Override
-      public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-        config.setAllowedMethods(Collections.singletonList("*"));
-        config.setAllowCredentials(true);
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setMaxAge(3600L);
-        return config;
-      }
-    };
   }
 
   @Bean
---
+++ src/main/java/com/jannkasper/auction/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.jannkasper.auction.controller;
+
+import com.jannkasper.auction.model.Customer;
+import com.jannkasper.auction.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+public class LoginController {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@RequestMapping("/user")
+	public Customer getUserDetailsAfterLogin(Principal user) {
+		List<Customer> customers = customerRepository.findByEmail(user.getName());
+		if (customers.size() > 0) {
+			return customers.get(0);
+		}else {
+			return null;
+		}
+		
+	}
+
+}
