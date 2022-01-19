4404f7b0a80550666f093e425b9656e4b1eae3e7
Jan Kasper <jann.kasper@gmail.com>
2022-01-19 08:11:54 +0100

    microservices

---
+++ department-service/src/main/java/com/jannkasper/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.jannkasper.department.repository;
+
+import com.jannkasper.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
